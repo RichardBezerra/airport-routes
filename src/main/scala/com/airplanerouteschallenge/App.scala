afab97edb53f66606098b2534ec8e930f4b1b735
Richard Bezerra <richardbezerra@gmail.com>
2021-04-06 23:59:01 +0100

    Assembly plug in added

--- src/main/scala/com/airplanerouteschallenge/App.scala
+++ src/main/scala/com/airplanerouteschallenge/App.scala
@@ -4,14 +4,15 @@ import scala.util.{Failure, Success}
 
 object App {
   def main(args: Array[String]): Unit = {
+
     val (finder, routes) = args match {
-      case Array(_, _, _, "with-returning-routes") => (DirectedCycleGraphFinder, ExampleRoutes.extendedRoutes)
+      case Array(_, _, "with-returning-routes") => (DirectedCycleGraphFinder, ExampleRoutes.extendedRoutes)
       case _ => (TopologicalOrderingFinder, ExampleRoutes.providedRoutes)
     }
 
-    finder.findShortestPath(routes, Airport(args(1)), Airport(args(2))) match {
+    finder.findShortestPath(routes, Airport(args(0)), Airport(args(1))) match {
       case Success(routes) =>
-        routes.foreach(r => println(s"${r.departure} -- ${r.arrival} (${r.durationHours})"))
+        routes.foreach(r => println(s"${r.departure.iataCode} -- ${r.arrival.iataCode} (${r.durationHours})"))
         println(s"time: ${routes.map(_.durationHours).sum}")
 
       case Failure(exception) => println(s"Error: $exception")
