c8194e39021185dce4ad5083b8982655a5a66d29
Richard Bezerra <richardbezerra@gmail.com>
2021-04-07 00:28:46 +0100

    Feedback improved

--- src/main/scala/com/airplanerouteschallenge/App.scala
+++ src/main/scala/com/airplanerouteschallenge/App.scala
@@ -5,6 +5,17 @@ import scala.util.{Failure, Success}
 object App {
   def main(args: Array[String]): Unit = {
 
+    if (args.length < 2) {
+      println("Please, inform a departure and a arrival airport. Options are:")
+
+      println(s"${ExampleRoutes
+        .providedRoutes
+        .flatMap(r => Set(r.arrival.iataCode, r.departure.iataCode))
+        .toSet
+        .reduce((a1, a2)  => s"$a1, $a2")}")
+      return
+    }
+
     val (finder, routes) = args match {
       case Array(_, _, "with-returning-routes") => (DirectedCycleGraphFinder, ExampleRoutes.extendedRoutes)
       case _ => (TopologicalOrderingFinder, ExampleRoutes.providedRoutes)
