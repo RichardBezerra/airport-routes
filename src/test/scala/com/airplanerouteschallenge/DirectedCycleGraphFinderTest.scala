c9c9c6daed8604ed9ffdd7673e831373c70c667e
Richard Bezerra <richardbezerra@gmail.com>
2021-04-06 22:59:45 +0100

    Tests decription fixed

--- src/test/scala/com/airplanerouteschallenge/DirectedCycleGraphFinderTest.scala
+++ src/test/scala/com/airplanerouteschallenge/DirectedCycleGraphFinderTest.scala
@@ -13,129 +13,15 @@ class DirectedCycleGraphFinderTest extends AnyFlatSpec with Matchers {
                                                 _: Airport,
                                                 _: Airport) => ???
 
-  "DurationDistanceTrackingMap" should "be created using a list of airports" in {
-    val airports = shortestPathFinderMock.airports(providedRoutes)
-
-    val durationDistanceTrackingMap = HoursTrack(airports)
-
-    durationDistanceTrackingMap should have size airports.size
-  }
-
-  it should "initiate departure airport duration tracking as 0" in {
-    val airports = shortestPathFinderMock.airports(providedRoutes)
-
-    val durationDistanceTrackingMap = HoursTrack(airports)
-
-    durationDistanceTrackingMap.setDurationToZero(Airport("DUB"))
-
-    durationDistanceTrackingMap(Airport("DUB")) should be(HoursTrackPathValue(Seq()))
-  }
-
-  it should "update duration tracking to given arrival if duration to get that does not exist" in {
-    val airports = shortestPathFinderMock.airports(providedRoutes)
-
-    val durationDistanceTrackingMap = HoursTrack(airports)
-
-    // act
-    durationDistanceTrackingMap
-      .overridePathToArrivalIfRouteIsFaster(HoursTrackPathValue(), Route(Airport("DUB"), Airport("LHR"), 2))
-
-    // assert
-    durationDistanceTrackingMap(Airport("LHR")) should
-      be(HoursTrackPathValue(Seq(Route(Airport("DUB"), Airport("LHR"), 2))))
-  }
-
-  it should "update duration tracking to given arrival if duration to get that is smaller then its current value" in {
-    val airports = shortestPathFinderMock.airports(providedRoutes)
-
-    val durationDistanceTrackingMap = HoursTrack(airports)
-
-    durationDistanceTrackingMap
-      .overridePathToArrivalIfRouteIsFaster(HoursTrackPathValue(),
-        Route(Airport("DUB"), Airport("LHR"), 2))
-
-    // act
-    durationDistanceTrackingMap
-      .overridePathToArrivalIfRouteIsFaster(HoursTrackPathValue(),
-        Route(Airport("SNN"), Airport("LHR"), 1))
-
-    // assert
-    durationDistanceTrackingMap(Airport("LHR")) should
-      be(HoursTrackPathValue(Seq(Route(Airport("SNN"), Airport("LHR"), 1))))
-  }
-
-  it should "update duration tracking to given arrival " +
-    "if duration to get that from other path is smaller then its current value" in {
-    val airports = shortestPathFinderMock.airports(providedRoutes)
-
-    val durationDistanceTrackingMap = HoursTrack(airports)
-
-    durationDistanceTrackingMap
-      .overridePathToArrivalIfRouteIsFaster(HoursTrackPathValue(),
-        Route(Airport("DUB"), Airport("SYD"), 21))
-
-    // act
-    durationDistanceTrackingMap
-      .overridePathToArrivalIfRouteIsFaster(HoursTrackPathValue(),
-        Route(Airport("DUB"), Airport("LHR"), 1))
-
-    durationDistanceTrackingMap
-      .overridePathToArrivalIfRouteIsFaster(durationDistanceTrackingMap(Airport("LHR")),
-        Route(Airport("LHR"), Airport("SYD"), 10))
-
-    // assert
-    durationDistanceTrackingMap(Airport("SYD")) should
-      be(HoursTrackPathValue(Seq(Route(Airport("DUB"), Airport("LHR"), 1),
-              Route(Airport("LHR"), Airport("SYD"), 10))))
-  }
-
-  it should "not update duration tracking to given arrival if duration to get that is bigger than its current value" in {
-    val airports = shortestPathFinderMock.airports(providedRoutes)
-
-    val durationDistanceTrackingMap = HoursTrack(airports)
-
-    durationDistanceTrackingMap.setDurationToZero(Airport("DUB"))
-
-    durationDistanceTrackingMap
-      .overridePathToArrivalIfRouteIsFaster(durationDistanceTrackingMap(Airport("DUB")),
-        Route(Airport("DUB"), Airport("LHR"), 2))
-
-    // act
-    durationDistanceTrackingMap
-      .overridePathToArrivalIfRouteIsFaster(durationDistanceTrackingMap(Airport("DUB")),
-        Route(Airport("DUB"), Airport("LHR"), 3))
-
-    // assert
-    durationDistanceTrackingMap(Airport("LHR")) should
-      be(HoursTrackPathValue(Seq(Route(Airport("DUB"),Airport("LHR"), 2))))
-  }
-
-  "Lazy Dijkstra Path Finder" should "return shortest duration to all airports" in {
-
-    // act
-    val path = DirectedCycleGraphFinder.findShortestPath(providedRoutes, Airport("DUB"), Airport("LAS"))
-
-    // assert
-    path match {
-      case Success(routes) =>
-        routes should be(Seq(
-          Route(Airport("DUB"), Airport("ORD"), 6),
-          Route(Airport("ORD"), Airport("LAS"), 2)
-        ))
-
-      case Failure(exception) => fail(exception)
-    }
-  }
-
-  it should "find shortest duration from a departure to an arrival" in {
+  "Directed Cycle Graph Finder" should "find shortest duration from a departure to an arrival" in {
 
     // act
     val shortestPath = DirectedCycleGraphFinder.findShortestPath(providedRoutes, Airport("DUB"), Airport("SYD"))
 
     // assert
     shortestPath match {
-      case Success(routes) =>
-        routes should be(
+      case Success(shortestPath) =>
+        shortestPath should be(
           Seq(Route(Airport("DUB"), Airport("LHR"), 1),
             Route(Airport("LHR"), Airport("BKK"), 9),
             Route(Airport("BKK"), Airport("SYD"), 11)))
@@ -144,27 +30,8 @@ class DirectedCycleGraphFinderTest extends AnyFlatSpec with Matchers {
     }
   }
 
-  it should "find shortest duration from a departure to an arrival when routes are expanded " +
-    "to include returning routes" in {
-    val expandedRoutes = addReturningRoutes(providedRoutes)
-
-    // act
-    val shortestPath = DirectedCycleGraphFinder.findShortestPath(expandedRoutes, Airport("SYD"), Airport("DUB"))
-
-    // assert
-    shortestPath match {
-      case Success(routes) =>
-        routes should be(
-          Seq(Route(Airport("SYD"), Airport("BKK"), 11),
-            Route(Airport("BKK"), Airport("LHR"), 9),
-            Route(Airport("LHR"), Airport("DUB"), 1)))
-
-      case Failure(failure) => fail(failure)
-    }
-  }
-
-  "ShortestPathFinder" should "find shortest duration from a departure to an arrival when routes are expanded " +
-    "to include returning routes" in {
+  it should "find shortest duration from a departure to an arrival " +
+    "when routes are expanded to include returning routes" in {
     val expandedRoutes = addReturningRoutes(providedRoutes)
 
     // act
@@ -182,7 +49,7 @@ class DirectedCycleGraphFinderTest extends AnyFlatSpec with Matchers {
     }
   }
 
-  it should "return a failure when arrival airport is unreachable from the departure" in {
+  it should "return a failure when there is not route from departure to arrival" in {
 
     val path = DirectedCycleGraphFinder
       .findShortestPath(providedRoutes, Airport("LAS"), Airport("DUB"))
@@ -214,7 +81,7 @@ class DirectedCycleGraphFinderTest extends AnyFlatSpec with Matchers {
       case Success(_) => fail()
     }
   }
-  
+
   it should "return a failure when arrival airport is not in the provided list" in {
 
     val path = DirectedCycleGraphFinder
@@ -224,6 +91,121 @@ class DirectedCycleGraphFinderTest extends AnyFlatSpec with Matchers {
       case Failure(failure) => failure should be (InvalidAirport)
       case Success(_) => fail()
     }
+  }
+
+  it should "provide a Dijkstra implementation that fills shortest duration for some airports " +
+    "until it gets to arrival" in {
+    val graph = shortestPathFinderMock.buildGraph(providedRoutes)
+
+    val allAirports = shortestPathFinderMock.airports(providedRoutes)
+
+    val hoursDistanceTracking = HoursTrack(allAirports)
+
+    hoursDistanceTracking.setDurationToZero(Airport("DUB"))
+
+    // act
+    LazyDijkstra.fillHoursTrack(graph, allAirports, Airport("DUB"), Airport("LAS"), hoursDistanceTracking)
+
+    // assert
+    hoursDistanceTracking(Airport("LAS")).routes should have size 2
+    hoursDistanceTracking(Airport("LAX")).routes should have size 0
+  }
+
+  "Hours Track" should "be created using a list of airports" in {
+    val airports = shortestPathFinderMock.airports(providedRoutes)
+
+    val hoursTrack = HoursTrack(airports)
+
+    hoursTrack should have size airports.size
+  }
+
+  it should "initiate departure airport duration tracking as 0" in {
+    val airports = shortestPathFinderMock.airports(providedRoutes)
+
+    val hoursTrack = HoursTrack(airports)
+
+    hoursTrack.setDurationToZero(Airport("DUB"))
+
+    hoursTrack(Airport("DUB")) should be(HoursTrackPathValue(Seq()))
+  }
+
+  it should "update duration tracking to given arrival if duration to get that does not exist" in {
+    val airports = shortestPathFinderMock.airports(providedRoutes)
+
+    val hoursTrack = HoursTrack(airports)
+
+    // act
+    hoursTrack
+      .overridePathToArrivalIfRouteIsFaster(HoursTrackPathValue(), Route(Airport("DUB"), Airport("LHR"), 2))
+
+    // assert
+    hoursTrack(Airport("LHR")) should
+      be(HoursTrackPathValue(Seq(Route(Airport("DUB"), Airport("LHR"), 2))))
+  }
+
+  it should "update duration tracking to given arrival if duration to get that is smaller then its current value" in {
+    val airports = shortestPathFinderMock.airports(providedRoutes)
+
+    val hoursTrack = HoursTrack(airports)
+
+    hoursTrack
+      .overridePathToArrivalIfRouteIsFaster(HoursTrackPathValue(),
+        Route(Airport("DUB"), Airport("LHR"), 2))
+
+    // act
+    hoursTrack
+      .overridePathToArrivalIfRouteIsFaster(HoursTrackPathValue(),
+        Route(Airport("SNN"), Airport("LHR"), 1))
+
+    // assert
+    hoursTrack(Airport("LHR")) should
+      be(HoursTrackPathValue(Seq(Route(Airport("SNN"), Airport("LHR"), 1))))
+  }
+
+  it should "update duration tracking to given arrival " +
+    "if duration to get that from other path is smaller then its current value" in {
+    val airports = shortestPathFinderMock.airports(providedRoutes)
+
+    val hoursTrack = HoursTrack(airports)
+
+    hoursTrack
+      .overridePathToArrivalIfRouteIsFaster(HoursTrackPathValue(),
+        Route(Airport("DUB"), Airport("SYD"), 21))
+
+    // act
+    hoursTrack
+      .overridePathToArrivalIfRouteIsFaster(HoursTrackPathValue(),
+        Route(Airport("DUB"), Airport("LHR"), 1))
+
+    hoursTrack
+      .overridePathToArrivalIfRouteIsFaster(hoursTrack(Airport("LHR")),
+        Route(Airport("LHR"), Airport("SYD"), 10))
+
+    // assert
+    hoursTrack(Airport("SYD")) should
+      be(HoursTrackPathValue(Seq(Route(Airport("DUB"), Airport("LHR"), 1),
+        Route(Airport("LHR"), Airport("SYD"), 10))))
+  }
+
+  it should "not update duration tracking to given arrival if duration to get that is bigger than its current value" in {
+    val airports = shortestPathFinderMock.airports(providedRoutes)
+
+    val hoursTrack = HoursTrack(airports)
+
+    hoursTrack.setDurationToZero(Airport("DUB"))
+
+    hoursTrack
+      .overridePathToArrivalIfRouteIsFaster(hoursTrack(Airport("DUB")),
+        Route(Airport("DUB"), Airport("LHR"), 2))
+
+    // act
+    hoursTrack
+      .overridePathToArrivalIfRouteIsFaster(hoursTrack(Airport("DUB")),
+        Route(Airport("DUB"), Airport("LHR"), 3))
+
+    // assert
+    hoursTrack(Airport("LHR")) should
+      be(HoursTrackPathValue(Seq(Route(Airport("DUB"),Airport("LHR"), 2))))
   }
 
   def addReturningRoutes(providedRoutes: Seq[Route]): Seq[Route] = {
